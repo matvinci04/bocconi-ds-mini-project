baceb819014f5956e152558fc0eb589fef9ec299
Mattia Vinci <mattiavinci2004@gmail.com>
2025-09-15 22:11:07 +0200

    Delete features.py

--- features.py
+++
@@ -1,9 +0,0 @@
-FEATURES = [
-    "study_hours","lecture_attendance","prev_gpa","part_time_job",
-    "social_hours","sleep_hours","practice_tests"]
-TARGET = "exam_score"
-
-def split_features_target(df):
-    X = df[FEATURES].copy()
-    y = df[TARGET].copy()
-    return X,y
