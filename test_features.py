231b5cfa85c2f2289d6e75f1fe3d76bae7561051
Mattia Vinci <mattiavinci2004@gmail.com>
2025-09-15 22:16:49 +0200

    Delete test_features.py

--- test_features.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from src.features import split_features_target,FEATURES,TARGET
-
-def test_split():
-    df=pd.DataFrame({f:[1,2] for f in FEATURES+[TARGET]})
-    X,y=split_features_target(df)
-    assert list(X.columns)==FEATURES
-    assert y.name==TARGET
